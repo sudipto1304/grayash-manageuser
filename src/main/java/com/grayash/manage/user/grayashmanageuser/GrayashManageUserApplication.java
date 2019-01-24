21fd4e475e8c784ed65c66a9ff6060c9d0e786c7
Sudipto Aich <sudipto.aich.osv@fedex.com>
2019-01-24 12:19:34 -0600

    initial files

---
+++ src/main/java/com/grayash/manage/user/grayashmanageuser/GrayashManageUserApplication.java
@@ -0,0 +1,14 @@
+package com.grayash.manage.user.grayashmanageuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrayashManageUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrayashManageUserApplication.class, args);
+	}
+
+}
+
